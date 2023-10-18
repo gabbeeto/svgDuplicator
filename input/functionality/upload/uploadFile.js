78539841b4970735e65b55c2b99cfec942cb7ba5
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-18 17:30:00 -0300

    add data

--- input/functionality/upload/uploadFile.js
+++ input/functionality/upload/uploadFile.js
@@ -1,3 +1,33 @@
-export function uploadFiles(){
-alert('uploading works');
+let index;
+let fileInput;
+let svgFiles;
+let fileReader;
+
+export function uploadFiles() {
+  index = this.dataset.index
+  fileInput = document.querySelector(`li[data-index='${index}'] input`);
+  svgFiles = fileInput.files;
+  fileReader = new FileReader();
+  loadSvgData(svgFiles.length);
+
 }
+
+function loadSvgData(length) {
+  console.log(length)
+  // base case
+  if (length < 1) {
+    return 'finished';
+  }
+  // recursive case
+  else {
+    fileReader.readAsText(svgFiles[length - 1]);
+
+    fileReader.onload = () => {
+      console.log(fileReader.result);
+
+
+      loadSvgData(length - 1);
+    }
+  }
+}
+
