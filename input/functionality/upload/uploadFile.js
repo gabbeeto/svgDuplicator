727aeb52f0b224320569ca794a4887100d80c705
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-20 17:46:11 -0300

    get the information of the vector images

--- input/functionality/upload/uploadFile.js
+++ input/functionality/upload/uploadFile.js
@@ -30,6 +30,10 @@ async function loadSvgDataAndImportAppropiateData() {
 }
 
 function parseSvgData(data) {
+  let svgValueOfSelect = document.querySelector('#base').value;
+  if (svgValueOfSelect == labelIndex) {
+    getBaseForLaterUse(data);
+  }
   if (label[labelIndex].vector) {
     data.forEach((currentData, index) => { parseVector(currentData, index) })
   }
@@ -37,13 +41,65 @@ function parseSvgData(data) {
     data.forEach((currentData, index) => { parseRaster(currentData, index) })
   }
 }
+function getBaseForLaterUse(data) {
+  let svgRegex = /<svg[A-Za-z0-9 ="\.:\/]+>/;
+  window.mainSvgElement = `${data[0].match(svgRegex)}`;
+}
+
+
+
+
+
+
+
+
+
+
 
 function parseVector(data, index) {
-  console.log(data);
+  // get the content inside the group element without the group element
   let groupRegex = new RegExp(`(?<=<g id="${label[labelIndex].name}">)[a-zA-Z0-9 \n\t<="-/\.,>]+(?=<\/g>)`, 'g');
   let groupElement = data.match(groupRegex);
-  console.log(groupElement);
+
+  // get the classes for all the elements inside the group
+  let classesRegex = /(?<=class=('|"))[a-zA-Z0-9 \n\t=\.\/#%\^&]+(?=("|'))/gm;
+  let classesForGroupElements = `${groupElement}`.match(classesRegex);
+
+  // getting the attributes of the classes of the elements and convert them into a style attribute with all the properties of the class
+  let styleElementToReplaceClasses = classesForGroupElements.map(currentClass => {
+    let attributeRegex = new RegExp(`(?<=\.${currentClass}( |\n|\t){( |\n|\t))[a-zA-z: \t\n#0-9,\.]+(?=})`, 'g');
+    return `style='${data.match(attributeRegex)}'`;
+  });
+
+  // seperate all the elements of the group
+  let seperateGroupsRegex = /<[a-zA-Z0-9 \n\t=\.\/#%\^&"-]+\/>/gm
+  let seperateGroupsElements = `${groupElement}`.match(seperateGroupsRegex);
+
+  // replace all the classes of elements of the seperated group with style attributes containing all the properties of the class
+  let groupElementsWithStyleElements = seperateGroupsElements.map((currentClass, index) => {
+    let classToReplace = classesForGroupElements[index];
+    let classAttributeRegex = RegExp(`class=('|")${classToReplace}('|")`, 'gm');
+    let groupElementWithStyleElement = `${currentClass}`.replace(classAttributeRegex, styleElementToReplaceClasses[index])
+    return `${groupElementWithStyleElement}`
+  })
+
+  // merge all the groups and put it inside the g element in a string
+  let groupElementWithGroupContent = `<g id="${index}">${groupElementsWithStyleElements.join(" \n")}</g>`;
+
+  // push to the main label array
+  label[labelIndex].content.push({ groupElement: groupElementWithGroupContent })
 }
+
+
+
+
+
+
+
+
+
+
+
 
 function parseRaster(data, index) {
   let newIdOrHref = `index${index}`;
@@ -55,9 +111,9 @@ function parseRaster(data, index) {
   let hrefRegex = /(?<=(href|Href|HREF)="#)[ \n,\.#%^&*\(\)a-zA-Z0-9]+(?=")/gm
   let hrefAttribute = `${useElement}`.match(hrefRegex);
 
-  //get image element
   let imageRegex = new RegExp(`<image( |\n|\t)+(width|Width)="[0-9]+"( |\n|\t)+(height|Height)="[0-9]+"( |\n|\t)+id="${hrefAttribute}"[ a-zA-Z=":\/;0-9,+]+\/>`, 'gm');
   let imageElement = data.match(imageRegex);
+  //get image element
 
   // replace id's and href's so we don't mix them up in the future
   const idRemover = /(?<=id=")[a-zA-Z0-9 +%\$\^&*\.\n\t.]+(?=")/gm
@@ -67,3 +123,4 @@ function parseRaster(data, index) {
   // push to the main label array
   label[labelIndex].content.push({ image: newImageElement, use: newUseElement })
 }
+
