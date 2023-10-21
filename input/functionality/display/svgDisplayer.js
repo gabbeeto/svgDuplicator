58aa2a936ceaa4d0ab045ab816a982484d39f4ac
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-21 16:52:03 -0300

    prepare function to display in the displayer

--- input/functionality/display/svgDisplayer.js
+++ input/functionality/display/svgDisplayer.js
@@ -1,4 +1,18 @@
 export function updateOnDisplayer() {
-console.log(this);
+  let indexForLabel = this.dataset.index;
+  let indexForSvgContent = this.value;
 
+  let currentLabel = window.label[indexForLabel];
+  let svgContent = currentLabel.content[indexForSvgContent];
+
+  if (svgContent && currentLabel.vector) {
+    let {groupElement} = svgContent;
+    console.log(groupElement)
+  }
+  else if (svgContent && currentLabel.vector == false) {
+    let {use:useElement} = svgContent;
+    let imageElement = `${svgContent.image}`;
+    console.log(imageElement)
+    console.log(useElement)
+  }
 }
