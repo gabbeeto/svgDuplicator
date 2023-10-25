fca1303d3724f6d98bd67b2b56a85df6d06c59a8
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-25 16:28:05 -0300

    add other elements in html for new options

--- input/startingValues.js
+++ input/startingValues.js
@@ -4,3 +4,11 @@ labelInput.value = '2';
 
 let rangeInput = document.querySelector(`input[type='range']`);
 rangeInput.value = 95;
+
+let face = document.querySelector('#displayContent select');
+face.value =0;
+
+
+let body = document.querySelector('#displayContent select:nth-of-type(2)');
+face.value =0;
+
