373418fc426581f589bc68db898fbaf4c81ba083
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-27 15:53:15 -0300

    add functionality to the download all data button function

--- input/functionality/download/downloadData.js
+++ input/functionality/download/downloadData.js
@@ -1,5 +1,15 @@
 export function downloadDataFromLabelObject(){
-alert('hey');
+let labelButJson = JSON.stringify(window.label);
+let jsonFile = new File([labelButJson], 'labelData.json', {type:"application/json"});
+let urlData = URL.createObjectURL(jsonFile)
 
+let anchorTag = document.createElement('a');
+anchorTag.href = urlData;
+anchorTag.download = 'labelData.json';
+document.body.append(anchorTag);
+anchorTag.click();
+
+document.body.removeChild(anchorTag);
+URL.revokeObjectURL(urlData);
 
 }
