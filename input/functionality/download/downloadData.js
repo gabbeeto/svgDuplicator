416420a87863f1688ffe742341cb0bfdfff9f791
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-10-27 16:05:32 -0300

    restrict what you can upload and add some comments to make it more understandable

--- input/functionality/download/downloadData.js
+++ input/functionality/download/downloadData.js
@@ -1,15 +1,18 @@
-export function downloadDataFromLabelObject(){
-let labelButJson = JSON.stringify(window.label);
-let jsonFile = new File([labelButJson], 'labelData.json', {type:"application/json"});
-let urlData = URL.createObjectURL(jsonFile)
+export function downloadDataFromLabelObject() {
+  // get label data and convert that into a url
+  let labelButJson = JSON.stringify(window.label);
+  let jsonFile = new File([labelButJson], 'labelData.json', { type: "application/json" });
+  let urlData = URL.createObjectURL(jsonFile)
 
-let anchorTag = document.createElement('a');
-anchorTag.href = urlData;
-anchorTag.download = 'labelData.json';
-document.body.append(anchorTag);
-anchorTag.click();
+  // create anchor tag with url of the label object and make the user click the button so the file generates
+  let anchorTag = document.createElement('a');
+  anchorTag.href = urlData;
+  anchorTag.download = 'labelData.json';
+  document.body.append(anchorTag);
+  anchorTag.click();
 
-document.body.removeChild(anchorTag);
-URL.revokeObjectURL(urlData);
+  // remove anchor tag and url
+  document.body.removeChild(anchorTag);
+  URL.revokeObjectURL(urlData);
 
 }
